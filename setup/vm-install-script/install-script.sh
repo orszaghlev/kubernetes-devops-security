8aafd0118725e8a7f2bba6793b46a72f21b6e1c5
orszaghlev <orszaghlev@gmail.com>
2025-04-28 14:25:25 +0200

    Update VM install script

--- setup/vm-install-script/install-script.sh
+++ setup/vm-install-script/install-script.sh
@@ -78,8 +78,8 @@ java -version
 mvn -v
 
 echo ".........----------------#################._.-.-JENKINS-.-._.#################----------------........."
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
+echo 'deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/' > /etc/apt/sources.list.d/jenkins.list
 apt update
 apt install -y jenkins
 systemctl daemon-reload

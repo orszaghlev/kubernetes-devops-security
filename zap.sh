74633cb6392cbd0a0d593d36f8feb6149c720593
orszaghlev <orszaghlev@gmail.com>
2025-05-06 13:40:27 +0200

    Update pom.xml

--- zap.sh
+++ zap.sh
@@ -4,12 +4,13 @@ PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].node
 
 chmod 777 $(pwd)
 echo $(id -u):$(id -g)
-docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap_report.html
+
+docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -c zap-rules -r zap_report.html
+exit_code=$?
 
     sudo mkdir -p owasp-zap-report
     sudo mv zap_report.html owasp-zap-report
 
-exit_code=$?
 echo "Exit Code: $exit_code"
 
     if [[ ${exit_code} -ne 0 ]]; then
